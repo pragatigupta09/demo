71b9dd4df4e93d004cb5e2944d77b940cf34c944
pragatigupta09 <pragatigp06@gmail.com>
2025-06-09 09:40:50 +0530

    initial

---
+++ prime.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class prime {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        boolean isPrime=true;
+        if(n<=1){
+            isPrime=false;
+        } else {
+            for(int i=2;i<=n/2;i++) {
+                if(n%i==0) {
+                    isPrime=false;
+                    break;
+                }
+            }
+        }
+        
+        if(isPrime) {
+            System.out.println(n + " is a prime no.");
+        } else {
+            System.out.println(n +" is not a prime no.");
+        }
+    }
+}
